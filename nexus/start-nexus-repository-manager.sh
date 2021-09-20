274f7280d93e735542cb33b5b9689807c0848545
Constantine Buzikov <raven.oscar@gmail.com>
2021-09-20 15:32:44 +0300

    csi workaround

--- nexus/start-nexus-repository-manager.sh
+++ nexus/start-nexus-repository-manager.sh
@@ -2,7 +2,7 @@
 #
 # Copyright:: Copyright (c) 2017-present Sonatype, Inc. Apache License, Version 2.0.
 #
-chown -R 200:200 /nexus-data/
+chown -R nexus:nexus /nexus-data/
 rm -rf /nexus-data/lock
 cd /opt/sonatype/nexus
-exec ./bin/nexus run
+su - nexus -c "exec ./bin/nexus run"
