d144a516963a9399df7978ae8d665d631b860fe2
Constantine Buzikov <raven.oscar@gmail.com>
2021-09-21 02:14:37 +0300

    user fix

--- nexus/start-nexus-repository-manager.sh
+++ nexus/start-nexus-repository-manager.sh
@@ -2,7 +2,6 @@
 #
 # Copyright:: Copyright (c) 2017-present Sonatype, Inc. Apache License, Version 2.0.
 #
-chown -R nexus:nexus /nexus-data/
 rm -rf /nexus-data/lock
 cd /opt/sonatype/nexus
 su - nexus -c "/bin/bash /opt/sonatype/nexus/bin/nexus run"
